4c1c5fc1f76e1150043e49c350473022da6dec8a
Ureniji <119366041+Ureniji@users.noreply.github.com>
2023-01-01 02:34:07 +0100

    collecting data through API

--- src/app.js
+++ src/app.js
@@ -1,2 +1,57 @@
-let apiKey = "a44e040af1ca83adf6248d96cfbf1be8";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+function formatDate(date) {
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+  let monthIndex = date.getMonth();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "Mai",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[monthIndex];
+  let currentDate = date.getDate();
+  let year = date.getFullYear();
+  return `${day}, ${month} ${currentDate}, ${year} `;
+}
+
+function displayWeatherDetails(response) {
+  console.log(response);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#temperature_max").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+  document.querySelector("#temperature_min").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+}
+
+let apiKey = "197ef3a642b76eef90e131866f74a0a0";
+let city = "Berlin";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayWeatherDetails);
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
